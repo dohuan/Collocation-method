2d3e5e7b88f113e7c8827f0b2a28a2eab103ec73
dohuan <dohuan@msu.edu>
2015-06-26 11:13:56 -0400

    062615 11:13am

--- code-parallel-sym/ortho_poly.m
+++ code-parallel-sym/ortho_poly.m
@@ -1,4 +1,16 @@
-function out = ortho_poly(order,dist_type)
+function out = ortho_poly(order,dinfo)
+%%
+% dist_type.name dist_type.mean dist_type.std
+syms x
+pd = makedist(dinfo.name,dinfo.mean,dinfo.std);
+h = @(x)pdf(pd,x);
 
 
+end
+
+function out = ortho_int(p1,p2,p,lim)
+    syms x
+    temp = @(x)p1*p2*p;
+    temp = matlabFunction(temp);
+    out = integral(@(x)temp,[lim(1) lim(2)]);
 end
